69361bdbc4a119ca336b481ad3556806b3a062e3
Olie 3 <therealolie@proton.me>
2024-02-18 20:41:25 +0100

    cleaned up the code, rewrote build.sh into a Makefile

---
+++ Makefile
@@ -0,0 +1,96 @@
+APP_NAME := termuxlauncher
+APP_VERSION := 1.2
+## main source directory
+SRC_MAIN_DIR := app/src/main
+## java source directory
+JAVA_SRC_DIR := ${SRC_MAIN_DIR}/java
+## android.jar location
+ANDROIDJAR := ${PREFIX}/share/java/android.jar
+ANDROIDMANIFEST := ${SRC_MAIN_DIR}/AndroidManifest.xml
+## generated binary directory
+BIN_DIR := bin
+# java classes directory
+CLASSES_DIR := ${BIN_DIR}/classes
+## Required packages
+REQ_PKG := ecj dx aapt apksigner openssl
+## Apk output diractory"
+BUILD_APK_PATH := ${EXTERNAL_STORAGE}/Download/buildAPKs
+## Java file list
+JAVA_FILE_LIST := $(shell find ${JAVA_SRC_DIR} -type f -name \*.java)
+KEYSTORE_DIR := .keystore
+KEY_PK8 := ${KEYSTORE_DIR}/key.pk8
+X509_PEM := ${KEYSTORE_DIR}/cert.x509.pem
+REQ_PEM := ${KEYSTORE_DIR}/req.pem
+KEY_PEM := ${KEYSTORE_DIR}/key.pem
+APK := ${APP_NAME}-${APP_VERSION}.apk
+APK_UNSIGNED := ${APP_NAME}.unsigned.apk
+APK_ALIGNED := ${APP_NAME}.aligned.apk
+OSSL := openssl
+
+.PHONY: all clean build install depcheck
+
+all: build
+	make install
+
+depcheck:
+	for x in ${REQ_PEM}; do \
+		[ "$$(command -v "$$x")" ] || \
+		pkg install "$$x"; \
+	done
+	[ "$$(command -v "openssl")" ] || pkg install openssl-tool
+
+clean: 
+	rm -rf ${BIN_DIR}
+
+install:
+	printf "%s\n%s\n%s\n" \
+		"/system/bin/cp ${BUILD_APK_PATH}/${APK} /data/local/tmp/install.apk" \
+		"/system/bin/pm install -g /data/local/tmp/install.apk" \
+		| rish
+
+build: clean ${JAVA_FILE_LIST} ${X509_PEM} ${KEYSTORE_DIR} ${BIN_DIR}
+	aapt package -v -f \
+		-M ${ANDROIDMANIFEST} \
+		-S ${SRC_MAIN_DIR}/res \
+		-J ${JAVA_SRC_DIR} \
+		-I ${ANDROIDJAR} \
+		-m
+	ecj -verbose \
+		-d ${CLASSES_DIR} \
+		-classpath ${ANDROIDJAR} \
+		-sourcepath ${JAVA_SRC_DIR} \
+		${JAVA_FILE_LIST}
+	dx --dex --verbose --output ${BIN_DIR}/classes.dex ${CLASSES_DIR}
+	aapt package -v -f \
+		-M ${ANDROIDMANIFEST} \
+		-S ${SRC_MAIN_DIR}/res \
+		-F ${BIN_DIR}/${APK_UNSIGNED}
+	cd ${BIN_DIR}; \
+		aapt add -f ${APK_UNSIGNED} classes.dex; \
+		zipalign 4 ${APK_UNSIGNED} ${APK_ALIGNED}; \
+		apksigner sign \
+			-in ${APK_ALIGNED} \
+			-out ${APK} \
+			-key ../${KEY_PK8} \
+			-cert ../${X509_PEM}; \
+		chmod 644 ${APK}; \
+		cp ${APK} ${BUILD_APK_PATH}/${APK};
+
+~/storage:
+	termux-setup-storage
+
+${BIN_DIR}:
+	mkdir -p "${BIN_DIR}"
+
+${KEYSTORE_DIR}:
+	mkdir -p "${KEYSTORE_DIR}"
+
+
+${X509_PEM}:
+	openssl genrsa -out ${KEY_PEM} 2048
+	openssl req -new -key ${KEY_PEM} -out ${REQ_PEM}
+	openssl x509 -req -days 10000 -in ${REQ_PEM} \
+		-signkey ${KEY_PEM} -out ${X509_PEM}
+	openssl pkcs8 -topk8 -outform DER -in ${KEY_PEM} \
+		-inform PEM -out ${KEY_PK8} -nocrypt
+
--- app/src/main/java/amsitlab/android/apps/termuxlauncher/MainActivity.java
+++ app/src/main/java/amsitlab/android/apps/termuxlauncher/MainActivity.java
@@ -1,5 +1,4 @@
 package amsitlab.android.apps.termuxlauncher;
-// vim:foldmethod=syntax
 
 
 import android.os.Bundle;
@@ -23,7 +22,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity
+{
 	public static final String LOG_TAG = "termux-applist";
 
 	public static final String EXTERNAL_PATH_NAME = "termuxlauncher";
@@ -44,8 +44,10 @@ public class MainActivity extends Activity{
 
 	private static final String LAUNCH_SCRIPT_START = ""
 		+ "launch(){\n"
-		+ "\tname=\"$(echo \"$1\" | tr 'A-Z' 'a-z' )\"\n"
-		+ "\tcase \"$name\" in\n"
+		+ "\tcmd=\"$(echo \"$1\" | tr 'A-Z' 'a-z' )\"\n"
+		+ "\tname=''\n"
+		+ "\tpkg=''\n"
+		+ "\tcase \"$cmd\" in\n"
 		+ "\t\t--help|-h)\n"
 		+ "\t\tprintf \"Usage:\\n\"\n"
 		+ "\t\tprintf \"\\tlaunch [appname]\\n\"\n"
@@ -60,24 +62,29 @@ public class MainActivity extends Activity{
 
 	private static final String LAUNCH_SCRIPT_END = ""
 		+ "\t\t--list|-l)\n"
-		+ "{{applications_list}}"
+		+ "\t\tprintf \"{{applications_list}}\"\n"
 		+ "\n"
 		+ "\t\t;;\n"
 		+ "\t\t*)\n"
-		+ "\t\tprintf \"Unknown command: type 'launch --help' for detail .\\n\"\n"
+		+ "\t\tprintf \"Unknown command: type 'launch --help' for detail.\\n\"\n"
 		+ "\t\treturn 1\n"
 		+ "\t\t;;\n"
 		+ "\tesac\n"
+		+ "\tif [ \"$name\" ]; then\n"
+		+ "\t\tam start -n \"$pkg\" --user 0 >/dev/null 2>&1\n"
+		+ "\t\techo Launching \\'\"$name\"\\'\n"
+		+ "\tfi\n"
 		+ "}";
 
-	/** termux intent */
+	/** termux intent **/
 	private static Intent myIntent;
 
-	/** Public external storage path name */
+	/** Public external storage path name **/
 	private static File sExternalPath = Environment.getExternalStoragePublicDirectory(EXTERNAL_PATH_NAME);
 
 	/** Launch file **/
 	private static File sLaunchFile;
+	private static File sFinalFile;
 
 	/** user's alias file **/
 	private static File sAliasFile;
@@ -85,143 +92,153 @@ public class MainActivity extends Activity{
 	private static List<String> aliasses;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		readAliasFile();
-		createLaunchFile();
-		createTermuxIntent();
-		startActivity(myIntent);
+		onAll();
 	}
 
 	@Override
-	public void onResume(){
+	public void onResume()
+	{
 		super.onResume();
-		readAliasFile();
-		createLaunchFile();
-		createTermuxIntent();
-		startActivity(myIntent);
+		onAll();
 	}
 
 	@Override
-	public void onPause(){
+	public void onPause()
+	{
 		super.onPause();
-		readAliasFile();
-		createLaunchFile();
+		onAll();
+	}
+
+	private void onAll()
+	{
+		try {
+			readAliasFile();
+		} catch(Exception e) {
+			Log.e(LOG_TAG,"Could not Read from " + sAliasFile.toString());
+		}
+		new Thread(){
+			public void run(){
+				createLaunchFile();
+			}
+		}.start();
 		createTermuxIntent();
 		startActivity(myIntent);
+
 	}
 
-	private void createTermuxIntent(){
-		if(null == myIntent){
+	private void createTermuxIntent()
+	{
+		if(null == myIntent)
 			myIntent = getPackageManager().getLaunchIntentForPackage("com.termux");
-		}
 	}
 
-	private static void createExternalPath(){
+	private static void createExternalPath()
+	{
 		if(!sExternalPath.exists()){
 			sExternalPath.mkdirs();
 		}
 	}
 
-	private static void readAliasFile(){
+	private static void readAliasFile()
+	{
 		sAliasFile = new File(sExternalPath,EXTERNAL_ALIAS_FILE_NAME);
 		aliassedAppNames = new ArrayList<String>();
 		aliasses = new ArrayList<String>();
-		if(!sAliasFile.exists()) return;
-		try {
-			Scanner reader = new Scanner(sAliasFile);
+		if(!sAliasFile.exists())
+			return;
+		try{
+			Scanner reader;
+			reader = new Scanner(sAliasFile);
 			while(reader.hasNextLine()) {
 				String line = reader.nextLine();
-				String[] data = line.split("=",2);
+				String trimmed = line.trim();
+				if(trimmed.charAt(0)=='#')
+					continue;
+				String[] data = trimmed.split("=",2);
 				aliasses.add(data[0]);
 				aliassedAppNames.add(data[1]);
 			}
 			reader.close();
-		} catch(Exception e) {
-			Log.e(LOG_TAG,"Could not Read from " + sAliasFile.toString());
-			return;
+		} catch (IOException e) {
+			Log.e(LOG_TAG,"couldnt read from" + EXTERNAL_ALIAS_FILE_NAME);
 		}
 		return;
-		
+
 	}
 
-	private void createLaunchFile(){
-		sLaunchFile = new File(sExternalPath,EXTERNAL_LAUNCH_FILE_NAME);
+	private String getAliassedName(String appName,String pkgName)
+	{
+		for(int i=0; i<aliassedAppNames.size();i++){
+			if(!appName.equals(aliassedAppNames.get(i).toLowerCase()))
+				continue;
+			return aliasses.get(i);
+		}
+		return appName;
+
+	}
+
+	private void createLaunchFile()
+	{
+		try{
+			sLaunchFile = new File(sExternalPath,".tmp");
+			sFinalFile = new File(sExternalPath,EXTERNAL_LAUNCH_FILE_NAME);
 
 
-		new Thread(){
-			public void run(){
+			// Always up to date
+			if(sLaunchFile.exists())
+				sLaunchFile.delete();
 
-				try{
-					// Always up to date
-					if(sLaunchFile.exists()){
-						sLaunchFile.delete();
-					}
+			createExternalPath();
+			final FileOutputStream fos = new FileOutputStream(sLaunchFile);
+			final PrintStream printer = new PrintStream(fos);
+			final PackageManager pm = getApplicationContext().getPackageManager();
+			List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+			printer.print(LAUNCH_SCRIPT_COMMENT);
+			printer.print(LAUNCH_SCRIPT_START);
 
-					createExternalPath();
-					final FileOutputStream fos = new FileOutputStream(sLaunchFile);
-					final PrintStream printer = new PrintStream(fos);
-					final PackageManager pm = getApplicationContext().getPackageManager();
-					List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-					printer.print(LAUNCH_SCRIPT_COMMENT);
-					printer.print(LAUNCH_SCRIPT_START);
-
-					final StringBuilder appNameList = new StringBuilder();
-					for(ApplicationInfo pkg : packages){
-						String pkgName = pkg.packageName;
-						String originalAppName = pkg.loadLabel(pm).toString();
-						String appName = originalAppName.toLowerCase();
-						Intent intent = pm.getLaunchIntentForPackage(pkgName);
-						boolean isSystemApp = ((pkg.flags & ApplicationInfo.FLAG_SYSTEM) == 1) ? true : false;
-						Log.d(LOG_TAG,"[" + intent + "] : [" + pkgName + "] : [" + isSystemApp + "] : [" + "] : [" + appName + "]");
-						if(intent == null){
-							continue;
-						}
-						String componentName = intent.getComponent().flattenToShortString();
-						String toRemove = "'\"()&{}$!<>#+*";
-						for(int i=0;i<toRemove.length();i++){
-							appName = appName.replace(""+toRemove.charAt(i),"");
-						}
-						appName = appName.replace(" ","-");
-						appNameList.append("\t\tprintf \"");
-						appNameList.append( appName );
-						appNameList.append("\\n\"\n");
-
-						printer.print("\t\t" + appName);
-
-						for(int i=0; i<aliassedAppNames.size();i++){
-							if(appName.equals(aliassedAppNames.get(i).toLowerCase())){
-								printer.print("|"+aliasses.get(i));
-							}
-						}
-						printer.print(""
-								+ ")\n"
-								+ "\t\tam start -n '"
-								+ componentName
-								+ "' --user 0 &> /dev/null\n"
-								+ "\t\tprintf \"Launching '"
-								+ originalAppName.replace("\"","\\\"")
-								+ "'\\n\"\n"
-								+ "\t\t;;\n"
-							     );
-
-					}
-
-					printer.print(LAUNCH_SCRIPT_END.replace("{{applications_list}}",appNameList.toString()));
-
-
-					printer.flush();
-					printer.close();
-					fos.flush();
-					fos.close();
-				} catch(IOException ioe) {
-					Log.e(LOG_TAG,"Could not write to " + sLaunchFile.toString());
-				}
+			final StringBuilder appNameList = new StringBuilder();
+			for(ApplicationInfo pkg : packages){
+				String pkgName = pkg.packageName;
+				String originalAppName = pkg.loadLabel(pm).toString();
+				String appName = originalAppName.toLowerCase();
+				appName = appName.split(":")[0];
+				Intent intent = pm.getLaunchIntentForPackage(pkgName);
+				boolean isSystemApp = ((pkg.flags & ApplicationInfo.FLAG_SYSTEM) == 1) ? true : false;
+				Log.d(LOG_TAG,"[" + intent + "] : [" + pkgName + "] : [" + isSystemApp + "] : [" + "] : [" + appName + "]");
+				if(intent == null)
+					continue;
+				String componentName = intent.getComponent().flattenToShortString();
+				String toRemove = "'\"()&{}$!<>#+*";
+				for(int i=0;i<toRemove.length();i++)
+					appName = appName.replace(""+toRemove.charAt(i),"");
+				toRemove = " \n\t";
+				for(int i=0;i<toRemove.length();i++)
+					appName = appName.replace(""+toRemove.charAt(i),"-");
+				String aliassedName = getAliassedName(appName,pkgName);
+				if(aliassedName.equals(""))
+					continue;
+				appNameList.append( aliassedName );
+				appNameList.append("\\n");
+				printer.print(appName
+						+ ")\n"
+						+ "\t\tname=\"" + originalAppName.replace("\"","\\\"") + "\"\n"
+						+ "\t\tpkg=\"" + componentName + "\"\n"
+						+ "\t\t;;\n"
+					     );
 
 			}
+			printer.print(LAUNCH_SCRIPT_END.replace("{{applications_list}}",appNameList.toString()));
+			printer.flush();
+			printer.close();
+			fos.flush();
+			fos.close();
+			sLaunchFile.renameTo(sFinalFile);
+		} catch(IOException ioe) {
+			Log.e(LOG_TAG,"Could not write to " + sLaunchFile.toString());
+		}
 
-		}.start();
 	}
-
-
 }
